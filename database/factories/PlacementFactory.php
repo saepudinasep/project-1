a53b8cec3f1abd54a4932c84d29245b9a51830ab
saepudinasep <saepudinasep2001@gmail.com>
2024-09-26 15:11:07 +0700

    [ADD] Menambahkan seeder

---
+++ database/factories/PlacementFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Placement>
+ */
+class PlacementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => fake(),
+            'branch_id' => fake(),
+            'kas_id' => fake(),
+        ];
+    }
+}
