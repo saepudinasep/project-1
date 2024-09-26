1fd2c683a80d2b2a4a9e0ed9356084295d2422e5
saepudinasep <saepudinasep2001@gmail.com>
2024-09-26 11:51:08 +0700

    [UPDATE] Merubah model

--- app/Models/Kas.php
+++ app/Models/Kas.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Kas extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
 }
--- app/Models/Placement.php
+++ app/Models/Placement.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Placement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_user',
+        'id_branch',
+        'id_kas',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function branch(): BelongsTo
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function kas(): BelongsTo
+    {
+        return $this->belongsTo(Kas::class);
+    }
 }
