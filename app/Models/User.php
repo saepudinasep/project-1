75cc431d5439a2bfe7ea75ee6fdac7285f40c8f1
saepudinasep <saepudinasep2001@gmail.com>
2024-09-26 15:28:44 +0700

    [UPDATE] Merubah model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +45,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id', 'id');
+    }
+
+    public function placements(): HasMany
+    {
+        return $this->hasMany(Placement::class, 'id_user', 'id');
     }
 }
