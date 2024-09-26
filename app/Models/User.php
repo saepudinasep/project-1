0552261b8880d9ef1067a1327476fd7d2656f827
saepudinasep <saepudinasep2001@gmail.com>
2024-09-26 11:22:55 +0700

    [ADD] Menambahkan migrate

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'nik',
         'password',
     ];
 
@@ -40,7 +40,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            // 'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
---
+++ database/migrations/2024_09_26_042103_create_placements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('placements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->unsignedBigInteger('id_branch');
+            $table->foreign('id_branch')->references('id')->on('branches');
+            $table->unsignedBigInteger('id_kas');
+            $table->foreign('id_kas')->references('id')->on('kas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('placements');
+    }
+};
